d8862a93f67f74b916cc1d66db354dd0207a320b
RebGov <becci.govert@gmail.com>
2018-09-05 14:10:03 -0500

    front so far... need to be in master

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,25 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+//user add pokemon in their team-card
+    //max 6
+  //user can release pokemon off of card(not delete from db)
+    //  click function event listner
+
+//when user loads page:
+    //see trainer name in each card
+function putTrainersOnPage(trainers) {
+  console.log(trainers)
+
+
+
+}
+
+
+
+//fetching info
+const parseJSON = resp => resp.json();
+
+fetch("http://localhost:3000/trainers")
+    .then(parseJSON)
+    .then(putTrainersOnPage)
